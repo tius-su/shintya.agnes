34c67d5bde6ee4c9f5207d5649f7db6976e0febd
tius-su <tiuss168@gmail.com>
2026-09-12 14:36:23 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'shintya-portfolio-v4';
+const CACHE_NAME = 'shintya-portfolio-v5';
 const ASSETS = [
   './',
   './index.html',
@@ -7,6 +7,11 @@ const ASSETS = [
   './tia.jpg',
   './icon-192.png',
   './icon-512.png',
+  './Freshmen Leader/Freshmen Leader1.jpeg',
+  './Freshmen Leader/Freshmen Leader2.jpeg',
+  './Freshmen Leader/Freshmen Leader3.jpeg',
+  './Freshmen Partner/Freshmen Partner.jpeg',
+  './Freshmen Partner/Freshmen Partner1.jpeg',
   'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
 ];
